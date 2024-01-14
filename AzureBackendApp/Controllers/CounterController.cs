0cce2b631086e19afb05f8ab09bca7273033be9a
LAPTOP-L1T0L126\micha <michalkolarzyk@gmail.com>
2024-01-14 15:54:39 +0100

    Add project files.

---
+++ AzureBackendApp/Controllers/CounterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureBackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        private static int _counter = 0;
+        [HttpGet]
+        public int Get()
+        {
+            return _counter;
+        }
+
+        // POST api/<CounterController>
+        [HttpPost("Increment")]
+        public void Increment()
+        {
+            _counter += 1;
+        }
+
+        [HttpPost("Decrement")]
+        public void Decrement()
+        {
+            if (_counter <= 0)
+                throw new Exception("Counter can not have negative value");
+
+            _counter -= 1;
+        }
+
+        // DELETE api/<CounterController>/5
+        [HttpDelete("{id}")]
+        public void Reset()
+        {
+            _counter = 0;
+        }
+    }
+}
