ffb6b62b4e310c77271f96d04313228d77d76e3e
LAPTOP-L1T0L126\micha <michalkolarzyk@gmail.com>
2024-01-14 16:21:18 +0100

    Add new endpoints

---
+++ AzureBackendApp/Controllers/EnvironmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureBackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        // GET: api/<EnvironmentController>
+        [HttpGet("Variables")]
+        public IDictionary Get()
+        {
+            return Environment.GetEnvironmentVariables();
+        }
+    }
+}
